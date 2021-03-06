c56d9705eb5609a0ccb9066c8740c9994fad49f2
Ken-you <asdr23@hotmail.com>
2021-03-06 14:49:54 +0800

    Initial Commit

---
+++ test0306/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test0306
+//
+//  Created by macbook3 on 2021/3/6.
+//  Copyright © 2021 ken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
